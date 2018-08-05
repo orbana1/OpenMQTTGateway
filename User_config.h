98419a1f2805e8ef6dfe58fa900071444ea26c4e
orbana1 <orbana1@gmail.com>
2018-08-05 11:55:03 +0200

    Update User_config.h

--- User_config.h
+++ User_config.h
@@ -36,7 +36,7 @@
 //#define mqtt_server_name "www.mqtt_broker.com" // instead of defining the server by its IP you can define it by its name, uncomment this line and set the correct MQTT server host name
 char mqtt_user[20] = "your_username"; // not compulsory only if your broker needs authentication
 char mqtt_pass[20] = "your_password"; // not compulsory only if your broker needs authentication
-char mqtt_server[40] = "192.168.1.17";
+char mqtt_server[40] = "192.168.0.122";
 char mqtt_port[6] = "1883";
 
 #define Gateway_Name "OpenMQTTGateway"
@@ -61,8 +61,8 @@ char mqtt_port[6] = "1883";
 
 // Update these with values suitable for your network.
 #if defined(ESP32) || defined(ESPWifiManualSetup) // for nodemcu, weemos and esp8266
-  #define wifi_ssid "wifi ssid"
-  #define wifi_password "wifi password"
+  #define wifi_ssid "honeypot"
+  #define wifi_password "maukope13"
 #else // for arduino + W5100
   const byte mac[] = {  0xDE, 0xED, 0xBA, 0xFE, 0x54, 0x95 }; //W5100 ethernet shield mac adress
 #endif
@@ -91,7 +91,7 @@ const byte subnet[] = { 255, 255, 255, 0 }; //ip adress
 
 #define ZgatewayRF     "RF"       //ESP8266, Arduino, ESP32
 //#define ZgatewayRF315  "RF315"    //ESP8266, Arduino, ESP32
-#define ZgatewayIR     "IR"       //ESP8266, Arduino,         Sonoff RF Bridge
+//#define ZgatewayIR     "IR"       //ESP8266, Arduino,         Sonoff RF Bridge
 #define ZgatewayBT     "BT"       //ESP8266, Arduino, ESP32
 //#define ZgatewayRF2    "RF2"      //ESP8266, Arduino, ESP32
 //#define ZgatewaySRFB   "SRFB"     //                          Sonoff RF Bridge
